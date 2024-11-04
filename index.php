d766faa8f206c7ea7015957fad843be790bc3b3c
thaivm-0609 <vuongthai95@gmail.com>
2024-11-04 08:47:16 +0700

    feat: add javascript

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="index.js"></script>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
     <p>Đây là heading</p>
